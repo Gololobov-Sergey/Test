3bd1cf3dbd40fd1b96d652852875ee9950e0b565
gololobov <gololobov.serhiy@gmail.com>
2024-04-19 15:36:48 +0300

    Добавьте файлы проекта.

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartedAssemblies = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.AvailableAssemblies = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Запущені процеси";
+            // 
+            // StartedAssemblies
+            // 
+            this.StartedAssemblies.FormattingEnabled = true;
+            this.StartedAssemblies.Location = new System.Drawing.Point(16, 40);
+            this.StartedAssemblies.Name = "StartedAssemblies";
+            this.StartedAssemblies.Size = new System.Drawing.Size(198, 316);
+            this.StartedAssemblies.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(241, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(241, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(241, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(241, 198);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // AvailableAssemblies
+            // 
+            this.AvailableAssemblies.FormattingEnabled = true;
+            this.AvailableAssemblies.Location = new System.Drawing.Point(365, 42);
+            this.AvailableAssemblies.Name = "AvailableAssemblies";
+            this.AvailableAssemblies.Size = new System.Drawing.Size(193, 303);
+            this.AvailableAssemblies.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Запущені процеси";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 387);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AvailableAssemblies);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.StartedAssemblies);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox StartedAssemblies;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox AvailableAssemblies;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAvailableAssemblies();
+
+            //comboBox1.Text = comboBox1.Items[0].ToString();
+            //this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+        }
+
+        void LoadAvailableAssemblies()
+        {
+            //название файла сборки текущего приложения
+            string except = new FileInfo(Application.ExecutablePath).Name;
+            //получаем название файла без расширения
+            except = except.Substring(0, except.IndexOf("."));
+            //получаем все *.exe файлы из домашней директории
+            string[] files = Directory.GetFiles(Application.StartupPath, "*.exe");
+            foreach (var file in files)
+            {
+                //получаем имя файла
+                string fileName = new FileInfo(file).Name;
+                /*если имя афйла не содержит имени исполняемого файла проекта,
+                *то оно добавляется в список*/
+                if (fileName.IndexOf(except) == -1)
+                    AvailableAssemblies.Items.Add(fileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(AvailableAssemblies.SelectedItem != null)
+            {
+                Process.Start(AvailableAssemblies.SelectedItem.ToString());
+            }
+        }
+
+
+
+        //private void btnAdd_Click(object sender, EventArgs e)
+        //{
+        //    string mess = "";
+        //    if (cbLangEn.Checked)
+        //        mess += cbLangEn.Text + " ";
+        //    if (cbLangFr.Checked)
+        //        mess += cbLangFr.Text;
+        //    MessageBox.Show(mess, "WinForm", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        //}
+
+        //private void cbLangEn_CheckedChanged(object sender, EventArgs e)
+        //{
+        //    cbLangFr.Enabled = cbLangEn.Checked;
+        //    if (!cbLangEn.Checked)
+        //        cbLangFr.Checked = false;
+
+        //}
+
+        //private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show((sender as ComboBox).SelectedItem.ToString());
+        //}
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    if (textBox1.Text != "")
+        //    {
+        //        comboBox1.Items.Add(textBox1.Text);
+        //        textBox1.Text = "";
+        //    }
+
+
+        //}
+
+        //private void Form1_MouseClick(object sender, MouseEventArgs e)
+        //{
+
+
+        //}
+
+        //private void Form1_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    this.Text = $"X - {e.X}, Y - {e.Y}";
+        //}
+    }
+}
